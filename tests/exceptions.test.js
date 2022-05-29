5a63e96b5a67778ede383614a88763a98d3e8764
berna39 <kalemajoseph39@gmail.com>
2022-05-29 21:33:38 +0200

    Mode tests for exceptions

--- tests/exceptions.test.js
+++ tests/exceptions.test.js
@@ -2,8 +2,9 @@ const gunnersCanPlayFor = require('../features/exceptions');
 
 test('Test on gunners', () => {
     expect(() => gunnersCanPlayFor('Tottenham')).toThrow(Error);
-});
-
-test('Test on gunners', () => {
     expect(() => gunnersCanPlayFor('Tottenham')).toThrow();
+
+    // mode details
+    expect(() => gunnersCanPlayFor('Tottenham')).toThrow('A gunner could never play for Spurs');
+    expect(() => gunnersCanPlayFor('Tottenham')).toThrow(/gunner/);
 });
