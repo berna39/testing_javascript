3606703247e047055c270ca92649b5dd5232b256
berna39 <kalemajoseph39@gmail.com>
2022-05-31 22:19:17 +0200

    Testing mock resluts

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -7,6 +7,11 @@ test('Test callback with mocking', () => {
 
     expect(mockCallBack.mock.calls.length).toBe(2);
 
+    //testing call states
     expect(mockCallBack.mock.calls[0][0]).toBe(3); // first argument on first call
     expect(mockCallBack.mock.calls[1][0]).toBe(5); // first argument on first call
+
+    //testing retun values
+    expect(mockCallBack.mock.results[0].value).toBe(42);
+    expect(mockCallBack.mock.results[1].value).toBe(44);
 });
