d1145ae9d653ac379fa99008d58c882132ac9136
berna39 <kalemajoseph39@gmail.com>
2022-05-31 22:12:11 +0200

    Other mocking call verifications

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -6,4 +6,7 @@ test('Test callback with mocking', () => {
     forEach([3,5], mockCallBack);
 
     expect(mockCallBack.mock.calls.length).toBe(2);
+
+    expect(mockCallBack.mock.calls[0][0]).toBe(3); // first argument on first call
+    expect(mockCallBack.mock.calls[1][0]).toBe(5); // first argument on first call
 });
