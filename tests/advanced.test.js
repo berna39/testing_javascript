85d0427ab88f3f779a0d724eff14bf4fab578f6a
berna39 <kalemajoseph39@gmail.com>
2022-05-31 19:42:44 +0200

    After event

--- tests/advanced.test.js
+++ tests/advanced.test.js
@@ -4,6 +4,11 @@ beforeEach(() => {
     console.log('Get everything prepared for test')
 });
 
+afterEach(() => {
+    console.log('Delete all dummy data used for test');
+});
+
 test('Make sure that foo returns something', () => {
     expect(foo()).toBe('I believe in God');
 });
+
